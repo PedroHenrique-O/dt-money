cfb06d5830e73b1f0504c1bdd9d9b4445e376f30
Pedro <pedrokaz.henrique@gmail.com>
2021-12-05 22:35:55 -0300

    added modal functions and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import { Header } from "./components/Header";
 import { Summary } from "./components/Summary";
 import { TransactionTable } from "./components/TransactionTable";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/newTransactionModal";
+import { TransactionProvider } from "./TransactionContext";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleClosenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
   return (
-    <>
-      <Header />
+    <TransactionProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Summary />
       <TransactionTable />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleClosenNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </TransactionProvider>
   );
 }
