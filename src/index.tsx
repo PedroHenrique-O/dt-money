8ca479e4d3d8843ce63fec198507470c45cbda07
Pedro <pedrokaz.henrique@gmail.com>
2021-12-05 22:36:22 -0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,56 @@
+import { createServer, Model } from "miragejs";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "dev",
+          amount: "6880",
+          createdAt: new Date("2021-02-12 09:00:30"),
+        },
+        {
+          id: 2,
+          title: "Jantar",
+          type: "withdraw",
+          category: "Comida",
+          amount: "6880",
+          createdAt: new Date("2021-02-12 10:00:30"),
+        },
+        {
+          id: 3,
+          title: "Filme",
+          type: "withdraw",
+          category: "Laser",
+          amount: "6880",
+          createdAt: new Date("2021-02-12 04:00:30"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transaction", (schema) => {
+      return schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      let data = JSON.parse(request.requestBody);
+      return schema.create(data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
