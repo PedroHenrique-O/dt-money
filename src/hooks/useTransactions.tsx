cc5108408f80a321e5ea973212f38c9ce3382665
Pedro <pedrokaz.henrique@gmail.com>
2021-12-06 00:07:02 -0300

    added hook

---
+++ src/hooks/useTransactions.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../components/services/api";
+
+import {
+  Transaction,
+  TransactionInput,
+  TransactionProviderProps,
+} from "../TransactionTypes";
+
+// type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
+interface TransactionContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+export const TransactionContext = createContext<TransactionContextData>(
+  {} as TransactionContextData
+);
+
+export function TransactionProvider({ children }: TransactionProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  // Setando em Transactions
+  useEffect(() => {
+    api
+      .get("/transaction")
+      .then((response) => setTransactions(response.data.transactions));
+    console.log("Estado:", transactions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //Inserção de dados na API
+
+  const createTransaction = async (transactionInput: TransactionInput) => {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
+  };
+
+  return (
+    <TransactionContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+
+  // eslint-disable-next-line no-unreachable
+}
+
+export function useTransactions() {
+  return useContext(TransactionContext);
+}
